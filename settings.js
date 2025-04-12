d08a3fc68c2fb66dab74159cc18bd55ce1ce957d
Muhammad <muhammadhabbibi24434@gmail.com>
2025-04-12 11:09:24 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Aizen',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Sosuke Bot",
+  botOwner: 'Aizen', // Your name
+  ownerNumber: '9021503942', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
